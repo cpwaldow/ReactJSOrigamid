546cbc8b51b144dc33b45898e4ecf286dac7dcc3
Carlos Eduardo <carlos.patricio@reclameaqui.com.br>
2021-07-19 21:16:21 -0300

    JSX Array

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Jsx from './Jsx';
+import JsxArray from './JsxArray';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Jsx />
+    <JsxArray />
   </React.StrictMode>,
   document.getElementById('root')
 );
