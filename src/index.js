0225397373a0b64cf440497711e98d0e3a6fe815
Carlos Eduardo <carlos.patricio@reclameaqui.com.br>
2021-07-20 20:15:36 -0300

    Eventos

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Jsx from './Jsx';
 import JsxArray from './JsxArray';
+import Eventos from './Eventos';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   {/*  <App />
     <Jsx />
     <JsxArray />
+    <Eventos /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
