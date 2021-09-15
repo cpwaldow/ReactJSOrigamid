d11608983b0703396124a36e0d67716c61105eb4
Carlos Eduardo <carlos.patricio@reclameaqui.com.br>
2021-09-14 21:13:44 -0300

    react hooks useState 1.2

--- src/hooksUseState/Modal.js
+++ src/hooksUseState/Modal.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Modal = ({ modal, setModal }) => {
-  console.log(modal)
-  if(modal) {
+  if (modal) {
     return (
       <div>
         Esse é um modal. <button onClick={() => setModal(false)}>Fechar</button>
       </div>
-    )
+    );
   } else {
-    return null
+    return null;
   }
-}
+};
 
-export default Modal
+export default Modal;
